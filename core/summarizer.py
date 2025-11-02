eda8e77dc6768889139958451faa27f0ad2cfb55
niveaaa <starader47@gmail.com>
2025-11-02 14:37:34 +0530

    fix: punkt_tab

--- core/summarizer.py
+++ core/summarizer.py
@@ -4,7 +4,25 @@ from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
 
+import nltk
+
 def summarize_text(text: str, sentence_count: int = 10) -> list:
+
+    try:
+        nltk.data.find("tokenizers/punkt")
+    except LookupError:
+        try:
+            nltk.download("punkt", quiet=True)
+        except:
+            pass
+
+    try:
+        nltk.data.find("tokenizers/punkt_tab")
+    except LookupError:
+        try:
+            nltk.download("punkt_tab", quiet=True)
+        except:
+            pass
    
     try:
         parser = PlaintextParser.from_string(text, Tokenizer("english"))
